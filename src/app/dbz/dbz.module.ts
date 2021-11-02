0a9f047dc90015d8981be918f308a8ea2d2f7fd9
Laura Juiz <ljuiz@southernhorizon.tech>
2021-11-02 14:21:46 -0300

    Fin S5 C59

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,24 @@
+/* Propio de Angular */
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+/* Terceros */
+
+/* Propio */
+import { MainPageComponent } from './main-page/main-page.component';
+
+
+@NgModule({
+  declarations: [
+    MainPageComponent
+  ],
+  exports: [
+    MainPageComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
+})
+export class DbzModule { }
---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+
+export class MainPageComponent {
+
+  personajes: Personaje[] = [
+    { nombre: 'Goku', poder: 15000 },
+    { nombre: 'Vegeta', poder: 7500 },
+    { nombre: 'Krillin', poder: 700 }
+  ];
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  };
+
+  agregar() {
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }
+
+    /*  Con el FormModule emite y recibe datos,
+        por lo tanto el this.nuevo ya está cargado
+        con la info agregada por el usuario al hacer submit
+    */
+    this.personajes.push(this.nuevo);
+
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+  }
+
+}
